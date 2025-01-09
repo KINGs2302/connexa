225006d360af5116ccbd24a9b5f7ba8905393f5f
KINGs2302 <jainishrupala@gmail.com>
2025-01-09 17:05:29 +0530

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 import './index.scss'
-import { router } from './Routes';
-import { ToastContainer, toast } from "react-toastify";
+import { router } from './Routes/Index';
+import { ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { app, auth  } from './Firebaseconfig';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
